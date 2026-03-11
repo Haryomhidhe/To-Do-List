d3c4f761d2941024faed07754af22ee35eac49e5
haryomhidhegoshen@gmail.com <haryomhidhegoshen@gmail.com>
2026-03-11 16:38:52 +0100

    update on to do list

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // 1. SELECTORS & VARIABLES
-const createBtn = document.querySelector('.create-btn');
-const taskListDiv = document.querySelector('#task-list');
+const createBtn = document.querySelector('#create-btn');
+const inboxCard = document.querySelector('#inbox-card');
+const urgentCard = document.querySelector('#urgent-card');
+const plannedCard = document.querySelector('#planned-card');
+const doneCard = document.querySelector('#done-card');
 const modal = document.querySelector('#task-modal');
 const saveBtn = document.querySelector('#save-btn');
 const cancelBtn = document.querySelector('#cancel-btn');
 const nameInput = document.querySelector('#modal-task-name');
 const categoryInput = document.querySelector('#modal-task-category');
+const date = document.querySelector('#date');
+const newcolumn = document.querySelector('#new-column');
+const board = document.querySelector('.board');
 // const editBtn = document.querySelector('.edit-btn');
 let tasks = []; // We move this to the top so everyone can use it
 let currentEditTask = null;
@@ -16,17 +22,35 @@ if (savedTasks) {
   tasks.forEach(function(task) {
     renderTask(task); // We use the blueprint to draw saved tasks
   });
-}
+};
+newcolumn.addEventListener('click', function(){
+  const columnName = prompt('Enter column name:');
+  console.log(columnName)
+  if(columnName){
+    const newCol = document.createElement('div')
+    newCol.className = 'column';
+    newCol.innerHTML = `
+  <h3>${columnName}</h3>
+  <div id="${columnName}-card"></div>`;
+  const newOption = document.createElement('option');
+newOption.value = columnName;
+newOption.textContent = columnName;
+categoryInput.appendChild(newOption);
+board.appendChild(newCol);
+  }
+  const customCard = document.querySelector(`#${columnName}-card`)
+});
+
 
 // 3. CREATE BUTTON LISTENER (The Input)
 createBtn.addEventListener('click', function() {
-   modal.classList.remove('hidden');
+   modal.classList.remove('hidden');``
 });
 
 saveBtn.addEventListener('click', function(){
   const taskName = nameInput.value;
   const taskCategory = categoryInput.value;
-
+  const taskDate = date.value;
   if (taskName) {
 
     if(currentEditTask){
@@ -37,6 +61,8 @@ saveBtn.addEventListener('click', function(){
       const newTask = {
         name: taskName,
         category: taskCategory,
+        date: taskDate,
+        originalCategory: taskCategory,
       }
         tasks.push(newTask);
       };
@@ -59,22 +85,37 @@ function renderTask(task) {
   const taskCard = document.createElement('div');
   taskCard.className = 'task-card';
   taskCard.innerHTML = `
+  <div class="check-box">
     <input type="checkbox">
     <span>${task.name}</span>
-    <small>(${task.category})</small>
+   </div> 
+    <span class="category-badge ${task.category} ">${task.category}</span>
+    <span>${task.date ? new Date(task.date).toLocaleString():'No date set '}</span>
+    <div class="card-buttons">
     <button class="delete-btn">Delete</button>
     <button class="edit-btn">Edit</button>
+    </div>
   `;
-
-  // B. Add Urgent Style
-  if (task.category === "Urgent") {
-    taskCard.style.borderLeft = "10px solid red";
-    taskCard.style.fontWeight = "bold";
+  if (task.category === "Inbox") {
+      inboxCard.appendChild(taskCard);
   }
+  if (task.category === "Urgent") {
+      urgentCard.appendChild(taskCard);
+  }
+  if (task.category === "Planned") {
+      plannedCard.appendChild(taskCard);
+  }
+  if (task.category === "Done") {
+      doneCard.appendChild(taskCard);
+  }else {
+  const customCard = document.querySelector(`#${task.category}-card`);
+  if (customCard) {
+    customCard.appendChild(taskCard);
+  }
+}
 
-  // C. Pin to Screen
-  taskListDiv.appendChild(taskCard);
 
+  
   // D. Delete Logic
 const deleteBtn = taskCard.querySelector('.delete-btn');
 
@@ -105,16 +146,23 @@ deleteBtn.addEventListener('click', function() {
   
   checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
-      taskText.style.textDecoration = 'line-through';
+      task.category = 'Done';
+      localStorage.setItem('myTasks',                  JSON.stringify(tasks));
+    renderTasks('Inbox');
     } else {
       taskText.style.textDecoration = 'none';
+      task.category = task.originalCategory;
+       localStorage.setItem('myTasks',                  JSON.stringify(tasks));
+    renderTasks('Inbox');
     }
   });
 }
 function renderTasks(category) {
   // 1. Clear the whiteboard
-  taskListDiv.innerHTML = '';
-  
+  inboxCard.innerHTML = '';
+  urgentCard.innerHTML = '';
+  plannedCard.innerHTML = '';
+  doneCard.innerHTML = '';  
   // 2. Decide which tasks to show (Inbox = Everything, Others = Filtered)
   const tasksToShow = (category === 'Inbox') ? tasks : tasks.filter(task => task.category === category);
 
